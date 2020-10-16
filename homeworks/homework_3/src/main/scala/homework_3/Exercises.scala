4e3410f1b82bb73c684c3baed8ddaf8f879b7971
Nagibator1488-SS <slava.shubakov@yandex.ru>
2020-10-16 15:59:14 +0500

    Exercises

--- homeworks/homework_3/src/main/scala/homework_3/Exercises.scala
+++ homeworks/homework_3/src/main/scala/homework_3/Exercises.scala
@@ -1,49 +1,62 @@
 package homework_3
 
 object Exercises {
+  def prettyBooleanFormatter1(x: Any): String = {
+    if (x.isInstanceOf[Boolean])
+      if (x.asInstanceOf[Boolean])
+        return "правда"
+      else
+        return "ложь"
+    else
+      return x.toString
+  }
 
+  def prettyBooleanFormatter2(x: Any): String = {
+    if(x.getClass == Class.forName("java.lang.Boolean"))
+      if (x.asInstanceOf[Boolean])
+        return "правда"
+      else
+        return "ложь"
+    else
+      return x.toString
+  }
 
-    /**
-     * Задание №1
-     * Реализуйте функцию, которая принимает любой тип и преобразует его в строку.
-     * Для всех типов кроме Boolean достаточно воспользоваться стандартной функцией .toString.
-     * Для типа Boolean сделайте особое преобразование: true -> "правда", false -> "ложь".
-     *
-     * Реализуйте функцию тремя разными способами, отличающимися тем, как определяется какой тип имеет значение переданное в аргументе. 
-     * Определение типа необходимо для реализации специальной логики работы с Boolean значениями, которая описана в условии выше.
-     */
-    def prettyBooleanFormatter1(x: Any): String = ???
+  def prettyBooleanFormatter3(x: Any): String = {
+    x match {
+      case true => "правда"
+      case false => "ложь"
+      case _ => x.toString
+    }
+  }
 
-    def prettyBooleanFormatter2(x: Any): String = ???
+  def max1(xs: Seq[Int]): Int = {
+    if (xs.isEmpty)
+      throw new UnsupportedOperationException
+    return xs.max
+  }
 
-    def prettyBooleanFormatter3(x: Any): String = ???
+  def max2(xs: Seq[Int]): Seq[Int] = {
+    if (xs.isEmpty)
+      return Seq()
+    return Seq(xs.max)
+  }
 
+  def max3(xs: Seq[Int]): Option[Int] = {
+    xs match {
+      case Seq() => None
+      case _ => Option(xs.max)
+    }
+  }
 
-    /**
-     * Задание №2
-     * Реализуйте функцию нахождения максимального числа в переданной коллекции интов (можно использовать все методы стандартной библиотеки).
-     *
-     * Реализуйте функцию тремя разными способами, отличающимися тем как функция себя ведет на пустой коллекции. 
-     * Обратите внимание на возвращаемые типы.
-     */
-    def max1(xs: Seq[Int]): Int = ???
+  def sumIntegers[CollectionType <: Iterable[Int]](xs: CollectionType): Int = xs.sum
 
-    def max2(xs: Seq[Int]): Seq[Int] = ???
+  def sum1(x: Int, y: Int): Int = sumIntegers(List(x, y))
 
-    def max3(xs: Seq[Int]): Option[Int] = ???
+  def sum2(x: Int, y: Int): Int = sumIntegers(Array(x,y).toIterable)
 
-    /**
-     * Задание №3
-     * Допустим дана функция sumIntegers, которая умеет суммировать числа.
-     */
-    def sumIntegers[CollectionType <: Iterable[Int]](xs: CollectionType): Int = xs.sum
+  def sum3(x: Int, y: Int): Int = sumIntegers(MyIterableClass(x, y))
+}
 
-    /**
-     * Реализуйте на основе нее 3 варианта суммирования 2х чисел, отличающиеся способом передачи этих 2х чисел в функцию sumIntegers.
-     * Как минимум одна из реализаций должна использовать тип данных (класс) написанный вами самостоятельно.
-     */ 
-    def sum1(x: Int, y: Int): Int = sumIntegers(???)
-    def sum2(x: Int, y: Int): Int = sumIntegers(???)
-    def sum3(x: Int, y: Int): Int = sumIntegers(???)
-
+case class MyIterableClass(x: Int, y: Int) extends Iterable[Int]{
+  override def iterator:Iterator[Int] = Iterator(x,y)
 }
