d7ae7d02e352690af4250c7208a661efeb78a03e
Nagibator1488-SS <slava.shubakov@yandex.ru>
2020-10-31 13:24:32 +0500

    Done homework_5

--- homeworks/homework_5/src/main/scala/Exercises.scala
+++ homeworks/homework_5/src/main/scala/Exercises.scala
@@ -8,16 +8,35 @@ object Exercises {
   case class Dog(override val name: String) extends Animal
 
 
+  case class Shelter[+T <: Animal](animals: List[T]) {
 
-  case class Shelter ...
+    def +[P >: T <: Animal](newAnimal: P): Shelter[P] = {
+      this.copy(newAnimal :: animals)
+    }
+
+    def ++[P >: T <: Animal](shelter: Shelter[P]): Shelter[P] = {
+      this.copy(animals ++ shelter.animals)
+    }
+
+    def getNames: List[String] = {
+      animals.map(_.name)
+    }
+
+    def feed[P >: T <: Animal](food: Food[P]): List[String] = {
+      animals.map(food.feed(_))
+    }
+  }
 
 
+  trait Food [T <: Animal] {
+    def feed[P <: T](animal: P): String = {
+      s"${animal.name} eats ${this.toString.toLowerCase()}"
+    }
+  }
 
-  trait Food ...
+  case object Meat extends Food[Animal]
 
-  case object Meat extends Food[Animal] ...
+  case object Milk extends Food[Cat]
 
-  case object Milk extends Food[Cat] ...
-
-  case object Bread extends Food[Dog] ...
+  case object Bread extends Food[Dog]
 }
